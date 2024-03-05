9bb78f59a5e082459d468275fa646a1a53f7a6c3
knowhatelse <kenan.kajtazovic.01@gmail.com>
2024-03-05 18:09:38 +0100

    KK - Created the Sort component

--- howlingGigglesFrontEnd/src/main.jsx
+++ howlingGigglesFrontEnd/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import Sort from './components/sort/Sort'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <p>Hello world</p>
+    <Sort></Sort>
   </React.StrictMode>,
   /*branch testing */
 )
