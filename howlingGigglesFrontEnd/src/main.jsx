6ffda1a468d1fd48a677b621baa52294acedf133
alemB1 <alem.belko@edu.fit.ba>
2024-03-05 21:24:11 +0100

    AB--clean-up-code

--- howlingGigglesFrontEnd/src/main.jsx
+++ howlingGigglesFrontEnd/src/main.jsx
@@ -5,9 +5,13 @@ import MainContent from './components/MainContent/MainContent.jsx'
 import './index.css'
 import MainCards from './components/CardsHolder/CardsHolder.jsx'
 import CardsHolder from './components/CardsHolder/CardsHolder.jsx'
+import Sort from './components/sort/Sort.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+    <NavBar />
+    <MainContent />
+    <Sort />
+    <CardsHolder />
   </React.StrictMode>,
 )
