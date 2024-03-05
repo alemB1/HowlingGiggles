10aba0bcab8dbef7261e49040d562561491fc314
knowhatelse <kenan.kajtazovic.01@gmail.com>
2024-03-05 23:18:06 +0100

    KK - Add dependencies

--- howlingGigglesFrontEnd/src/main.jsx
+++ howlingGigglesFrontEnd/src/main.jsx
@@ -4,7 +4,7 @@ import NavBar from './components/NavBar/NavBar.jsx'
 import MainContent from './components/MainContent/MainContent.jsx'
 import './index.css'
 import CardsHolder from './components/CardsHolder/CardsHolder.jsx'
-import Sort from './components/sort/Sort.jsx'
+import Sort from './components/Sort/Sort.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
