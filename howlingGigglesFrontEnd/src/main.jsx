b546c7e61a6226e4bb93327c0a2a3494bfaae2e3
brileeH <brilebrix123@gmail.com>
2024-03-06 02:58:41 +0100

    Post message redirect

--- howlingGigglesFrontEnd/src/main.jsx
+++ howlingGigglesFrontEnd/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import PostPage from './components/PostPage/PostPage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <p>Hello world</p>
+    <PostPage />
   </React.StrictMode>,
   /*branch testing */
 )
