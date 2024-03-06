a39553410d2c8aa5dd8dde70fe53d4368f61a859
alemB1 <alem.belko@edu.fit.ba>
2024-03-06 18:18:37 +0100

    AB--AddFooterComponent

--- howlingGigglesFrontEnd/src/main.jsx
+++ howlingGigglesFrontEnd/src/main.jsx
@@ -5,11 +5,10 @@ import MainContent from './components/MainContent/MainContent.jsx'
 import './index.css'
 import CardsHolder from './components/CardsHolder/CardsHolder.jsx'
 import Sort from './components/sort/Sort.jsx'
-import PostPage from './components/PostPage/PostPage.jsx'
+import Footer from './components/Footer/Footer.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     
-    <PostPage />
   </React.StrictMode>,
 )
